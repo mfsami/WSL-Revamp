59f43e68828e304ac85a780804d5b2920990f247
Sami El-sayed <samielsayed39@gmail.com>
2024-09-27 20:20:57 -0600

    Started scores and whatnot

--- src/components/ScoreFront.jsx
+++ src/components/ScoreFront.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "../styles/ScoreFront.css";
 
 const ScoreFront = () => {
-  return <div>ScoreFront</div>;
+  return (
+    <div className="container">
+      <div className="upcomingContainer">
+        <div className="line left"></div>
+        <p className="upcoming">UPCOMING MATCHES</p>
+        <div className="line right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ScoreFront;
