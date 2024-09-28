f639f99330c1a26b5e352472165d03312347f7b2
Sami El-sayed <samielsayed39@gmail.com>
2024-09-28 17:39:06 -0600

    Upcoming matches ui finished

--- src/pages/Home/Upcoming.jsx
+++ src/pages/Home/Upcoming.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import "../../styles/HomeStyles/Upcoming.css";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 
 const ScoreFront = () => {
   return (
     <div className="container">
+      {/* Upcoming matches top part dont touch */}
       <div className="upcomingContainer">
         <div className="line left"></div>
         <p className="upcoming">UPCOMING MATCHES</p>
         <div className="line right"></div>
+      </div>
+
+      <div className="datesContainer">
+        <p>12/27</p>
+        <p>12/27</p>
+        <p>12/27</p>
+        <p>12/27</p>
+        <p>12/27</p>
+        <SlArrowLeft className="mx-[-50px]" />
+        <div className="currentDateContainer">
+          <h1 className="text-6xl font-medium">12/27</h1>
+          <h1 className="text-2xl font-medium">Tuesday</h1>
+        </div>
+        <SlArrowRight className="mx-[-50px]" />
+        <p>12/27</p>
+        <p>12/27</p>
+        <p>12/27</p>
+        <p>12/27</p>
+        <p>12/27</p>
       </div>
     </div>
   );
