0e50ac476c2f93b85f87b7f1a076cccffc1c9f97
Sami El-sayed <samielsayed39@gmail.com>
2024-10-01 11:55:19 -0600

    Footer structure complete

--- src/pages/Home/Footer.jsx
+++ src/pages/Home/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../styles/HomeStyles/Footer.css";
 import NOTEXTLOGOGREY from "../../assets/images/NOTEXTLOGOGREY.png";
+import footerLogos from "../../assets/images/footerLogos.png";
 
 const Footer = () => {
   return (
@@ -9,6 +10,13 @@ const Footer = () => {
         <div className="lineFoot left"></div>
         <img className="footerLogo" src={NOTEXTLOGOGREY} alt="/" />
         <div className="lineFoot right"></div>
+      </div>
+      <div className="footerLogosCont">
+        <img className="footerLogosImg" src={footerLogos} alt="/" />
+      </div>
+      <div className="secondFootCont"></div>
+      <div className="bottomFootCont">
+        <p>@WSL 2025 CREATED BY SAMI-ELSAYED</p>
       </div>
     </div>
   );
