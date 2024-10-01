a4540aa9f13727d21edfea22dd8f08444d9e055d
Sami El-sayed <samielsayed39@gmail.com>
2024-09-30 18:58:32 -0600

    Finished eia image

--- src/pages/Home/EiaBg.jsx
+++ src/pages/Home/EiaBg.jsx
@@ -5,6 +5,12 @@ import EIA from "../../assets/images/eia.png";
 const EiaBg = () => {
   return (
     <div className="EiaBgCont">
+      <div class="locationText">
+        <p>At the</p>
+        <h1>Bernabeu</h1>
+        <h1>Academy</h1>
+        {/* <p>Edmonton</p> */}
+      </div>
       <img className="eiaBGimg" src={EIA} alt="/" />
     </div>
   );
