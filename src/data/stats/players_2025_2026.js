79daaba14b04265ebc5a751f337861a1a9e82ee1
Sami El-sayed <samielsayed39@gmail.com>
2025-12-23 15:09:28 -0700

    Player stat update

--- src/data/stats/players_2025_2026.js
+++ src/data/stats/players_2025_2026.js
@@ -15,7 +15,7 @@ export const players20252026 = [
     name: "Mohammed Sheikh",
     team: "DEMBELE FC",
     position: "BOTH",
-    goals: 0,
+    goals: 1,
     assists: 0,
     yellowCards: 0,
     redCards: 0,
